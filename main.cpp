e04ed57802f7babbd396d0fe3868e2440a61d4dc
uvasqve <91509126+uvasqve@users.noreply.github.com>
2022-06-03 10:20:49 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
 
+#include "Node.h"
+#include "Instance.h"
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	int number_of_features = 0;
+	int algorithm_selection = 0;
+	cout << "Welcome to Bertie Woosters (change this to your name) Feature Selection Algorithm" << endl;
+	cout << "Please enter total number of features: ";
+	cin >> number_of_features;
+	cout << "Type the number of the algorithm you want to run." << endl;
+	cout << "Forward Selection" << endl
+		<< "Backward Elimination" << endl
+		<< "My special algorithm" << endl;
+	cin >> algorithm_selection;
+	Node n;
+	n.featureSearch(number_of_features);
+	return 0;
+}
